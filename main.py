a240bcf4b5afb537a7c2b9ece470b95b9908290f
Chantal <chantal.shirley@pearson.com>
2021-11-15 17:59:50 -0600

    Fast API's status code decorators

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.param_functions import Body
 from pydantic import BaseModel
 from random import randrange
@@ -39,7 +39,7 @@ def root():
 def get_posts():
     return {"data": my_posts } 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
@@ -52,7 +52,12 @@ def get_latest_post():
     return {"detail": post}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message ' f"post with id: {id} not found :("}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} not found :(")
     return {"post_detail": post}
 
