292c09b13c98edefa89590b372dfa598744c3011
Chantal <chantal.shirley@pearson.com>
2021-11-19 16:28:43 -0600

    Response Model orm updates with Pydantic

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 import psycopg
 import yaml
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -18,11 +18,6 @@ with open('config.yaml', 'r') as file:
     user = raw_yml["user"]
     host = raw_yml["host"]
     dbname = raw_yml["dbname"]
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 # Connect to existing DB
 try:
@@ -69,7 +64,7 @@ def get_posts(db: Session = Depends(get_db)):
 
     posts = db.query(models.Post).all()
 
-    return {"data": posts} 
+    return posts # FastApi serializes the model into json
 
 # @app.get("/sqlalchemy")
 # def test_posts(db: Session = Depends(get_db)):
@@ -77,8 +72,8 @@ def get_posts(db: Session = Depends(get_db)):
 
 #     return {"status": posts}
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # post_dict = post.dict()
     # post_dict['id'] = randrange(0, 10000000)
     # my_posts.append(post_dict)
@@ -96,7 +91,7 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
 
 @app.get("/posts/latest")
 def get_latest_post():
@@ -114,7 +109,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
         # return {'message ' f"post with id: {id} not found :("}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} not found :(")
-    return {"post_detail": post}
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)): 
@@ -138,10 +133,10 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
     db.commit()
 
-    return {"detail": post}
+    return post
 
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # index = find_index_post(id)
     # cursor.execute(""" UPDATE posts SET title= %s, content = %s, published = %s WHERE id = %sRETURNING * """, 
     #                 (post.title, post.content, post.published, str(id)))
@@ -160,4 +155,4 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
     db.commit()
 
-    return {'data': post_query.first() }
+    return post_query.first() 
---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+
+from pydantic.main import BaseModel
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(BaseModel):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
