e1f55edeef8801fdeb502fd42ca7d33b34d7551e
Chantal <Chantal.C.Shirley.14@dartmouth.edu>
2022-01-09 17:03:22 -0600

    feat: Alembic added for db migrations/revisions

---
+++ alembic/versions/d01ba611108c_feat_create_post_table.py
@@ -0,0 +1,24 @@
+"""feat: create post table
+
+Revision ID: d01ba611108c
+Revises: 
+Create Date: 2022-01-09 16:52:55.471874
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd01ba611108c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
