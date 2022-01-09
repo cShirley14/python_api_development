42fd68a0094078363f4a5413ac38e989bc937db8
Chantal <Chantal.C.Shirley.14@dartmouth.edu>
2022-01-09 13:08:56 -0600

    Removed superfluous print statements and updated training video marker

--- app/routers/post.py
+++ app/routers/post.py
@@ -29,7 +29,6 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
         models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(
             models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
-    print(results)
     return results # FastApi serializes the model into json
 
 # @app.get("/sqlalchemy")
@@ -53,19 +52,20 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
     #                         published=post.published)
 
     new_post = models.Post(owner_id=current_user.id, **post.dict())
-    print(current_user)
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
     return new_post
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db),
             current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = (%s) """, (id,))
     # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, 
+        models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
 
     
     if not post:
